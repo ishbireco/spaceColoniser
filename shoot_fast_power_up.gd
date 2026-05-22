6df3e5e886a2925c989effa80bebdcfa441bb47b
kopedadud <ishbireco@gmail.com>
2026-05-22 22:52:25 +0530

    Update shoot_fast_power_up.gd

--- shoot_fast_power_up.gd
+++ shoot_fast_power_up.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 var bullet = preload("res://bullet.tscn")
 var shoot_fast_powerup = preload("res://shoot_fast_power_up.tscn")
+@onready var wave_label : RichTextLabel = $RichTextLabel
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
@@ -10,5 +11,6 @@ func _on_area_entered(area: Area2D) -> void:
 		
 func _ready() -> void:
 	if Waves.wave == 4:
-		shoot_fast_powerup.instantiate()
-		get_parent().add_child(shoot_fast_powerup)
+		var power_instance = shoot_fast_powerup.instantiate()
+		power_instance.global_position = Vector2(randi_range(0, 480), randi_range(0, 720))
+		get_tree().get_current_scene().add_child(power_instance)
